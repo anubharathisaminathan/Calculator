bbb6594e8321cef123e434c45ed12dd9dc92156b
anubharathisaminathan <80959254+anubharathisaminathan@users.noreply.github.com>
2021-11-22 17:29:52 +0530

    Update CalcGUI.java
    Updated Arithmetic calculator

--- Calculator/CalcGUI.java
+++ Calculator/CalcGUI.java
@@ -9,29 +9,33 @@ public class CalcGUI
 {
 	public static void main(String args[])
 	{
-		GUI obj = new GUI();
+		GUI1 obj = new GUI1();
 	}
 }
-class GUI extends JFrame implements ActionListener
+ class GUI1 extends JFrame implements ActionListener
 {
 	JTextField t1;
-	JLabel li,lt,lp,ll,l,lti;
-	JButton b;
-	JRadioButton b1,b2,b3,b4,b5,b6,b_1,b_2,b_3,b_4,b_5,b_6,b7,b8,b9,b10,b11,b12;
-	JPanel panel=new JPanel();
-	public GUI()
+	JLabel li,lt,lp,ll,l,lti,lpi;
+	//JButton bmat;
+	JRadioButton b,b1,b2,b3,b4,b5,b6,b_1,b_2,b_3,b_4,b_5,b_6,b7,b8,b9,b10,b11,b12,b13;
+	//JPanel panel=new JPanel();
+	public GUI1()
 	{
 		t1 =new JTextField(20);
+		t1.setText("\"Enter your expression here\"");
 		
-		li=new JLabel("This calculator is to string expression");
-		lt=new JLabel("Trigonometric calculations :");
-		lti=new JLabel("Inverse Trigonometric calculations:"); 
-		lp=new JLabel("Root calculations:");
-		ll=new JLabel("Logarithmic calculations:");
+		li=new JLabel("ARITHMETIC CALCULATOR");
+		li.setForeground(Color.WHITE);
+		
+		lt=new JLabel(">> Trigonometric calculations :");
+		lti=new JLabel(">> Inverse Trigonometric calculations:"); 
+		lp=new JLabel(">> Root calculations:");
+		ll=new JLabel(">> Logarithmic calculations:");
+		lpi=new JLabel(">> Expression:");
 		l= new JLabel("Result will be displayed here");
 		
 		
-		b= new JButton("COMPUTE ");
+		b=new JRadioButton("Evaluate expression");
 		
 		b1=new JRadioButton("sin");
 		b2=new JRadioButton("cos");
@@ -55,33 +59,39 @@ class GUI extends JFrame implements ActionListener
 		b11=new JRadioButton("log");
 		b12=new JRadioButton("ln");
 		
+		b13=new JRadioButton("Postfix");
+		
 		JPanel introp=new JPanel();
-		introp.setLayout(new FlowLayout(FlowLayout.CENTER,90,5));
+		introp.setLayout(new FlowLayout(FlowLayout.CENTER,200,5));
+		introp.setBackground(Color.RED);
 		this.add(introp);
 		
 		JPanel panel=new JPanel(new GridLayout());
 		panel.setLayout(new FlowLayout(FlowLayout.CENTER,10,5));
 		this.add(panel,BorderLayout.CENTER);
+		
 		panel.setBackground(Color.GRAY);
 		
-		JPanel p = new JPanel();
-		p.setLayout(new FlowLayout(FlowLayout.CENTER,10,5));
+		JPanel p = new JPanel();	
 		this.add(p);
 		
 		JPanel p_0 = new JPanel();
-		p_0.setLayout(new FlowLayout(FlowLayout.CENTER,10,5));
 		this.add(p_0);
 		
 		JPanel p1 = new JPanel();
-		p1.setLayout(new FlowLayout(FlowLayout.CENTER,10,5));
 		this.add(p1);
 		
 		JPanel p2=new JPanel();
-		p2.setLayout(new FlowLayout(FlowLayout.CENTER,10,5));
 		this.add(p2);
 		
+		JPanel p3=new JPanel();
+		this.add(p3);
+		
+		JPanel p4=new JPanel();
+		this.add(p4);
+		
 		JPanel pn = new JPanel();
-		pn.setLayout(new FlowLayout(FlowLayout.CENTER,200,5));
+		pn.setLayout(new FlowLayout(FlowLayout.CENTER,250,5));
 		this.add(pn);
 		pn.setBackground(Color.ORANGE);
 		
@@ -106,11 +116,12 @@ class GUI extends JFrame implements ActionListener
 		bg.add(b10);
 		bg.add(b11);
 		bg.add(b12);
+		bg.add(b13);
+		bg.add(b);
 		
 		introp.add(li);
 		
 		panel.add(t1);
-		panel.add(b);
 		
 		p.add(lt);
 		p.add(b1);
@@ -138,11 +149,15 @@ class GUI extends JFrame implements ActionListener
 		p2.add(b11);
 		p2.add(b12);
 		
+		p3.add(lpi);
+		p3.add(b13);
+		
+		p4.add(b);
 		pn.add(l);
 		
 
 		
-		b.addActionListener(this);//interface
+		//b.addActionListener(this);//interface
 		b1.addActionListener(this);
 		b2.addActionListener(this);
 		b3.addActionListener(this);
@@ -164,54 +179,70 @@ class GUI extends JFrame implements ActionListener
 		
 		b11.addActionListener(this);
 		b12.addActionListener(this);
-
-		setTitle("CALCULATOR");
-		setLayout(new FlowLayout());
+		
+		b13.addActionListener(this);
+		
+		b.addActionListener(this);
+		
+		
+		
+		setTitle("ARITHMETIC CALCULATOR");
+		this.setLayout(new FlowLayout());
 		setVisible(true);
-		setSize(650,400);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(700,10,650,400);		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
 	public void actionPerformed(ActionEvent ae)
 	{
+		infixtopostfix itp=new infixtopostfix();
 		String str = t1.getText();
 		double result;
-		//String str = t1.getText();
-		//String[] ch = str.split("+\\-\\*\\/");
-		int res = this.evaluate(str);
 		
 		if(b1.isSelected())
 		{
+//			int res = evaluateon(str);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			double rad =Math.toRadians(res);
 			result=Math.sin(rad);
 			l.setText(result+"");
 		}
 		else if(b2.isSelected())
 		{
+//			int res = evaluateon(str);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			double rad =Math.toRadians(res);
 			result=Math.cos(rad);
 			l.setText(result+"");
 		}
 		else if(b3.isSelected())	
 		{
+//			int res = evaluateon(str);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			double rad =Math.toRadians(res);
 			result=Math.tan(rad);
 			l.setText(result+"");
 		}
 		else if(b4.isSelected())
 		{
+//			int res = evaluateon(str);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			double rad =Math.toRadians(res);
 			result=(1/Math.sin(rad));
 			l.setText(result+"");
 		}
 		else if(b5.isSelected())
 		{
+//			int res = evaluateon(str);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			double rad =Math.toRadians(res);
 			result=(1/Math.cos(rad));
 			l.setText(result+"");
 		}
 		else if(b6.isSelected())
 		{
+//			int res = evaluateon(str);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			double rad =Math.toRadians(res);
 			result=(1/Math.tan(rad));
 			l.setText(result+"");
@@ -219,194 +250,92 @@ class GUI extends JFrame implements ActionListener
 		
 		else if(b_1.isSelected())
 		{
-			//double rad =Math.toRadians(res);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=Math.asin(res);
 			l.setText(result+"");
 		}
 		else if(b_2.isSelected())
 		{
-			//double rad =Math.toRadians(res);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=Math.acos(res);
 			l.setText(result+"");
 		}
 		else if(b_3.isSelected())	
 		{
-			//double rad =Math.toRadians(res);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=Math.atan(res);
 			l.setText(result+"");
 		}
 		else if(b_4.isSelected())
 		{
-			//double rad =Math.toRadians(res);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=(Math.asin((double)1/res));
 			l.setText(result+"");
 		}
 		else if(b_5.isSelected())
 		{
-			//double rad =Math.toRadians(res);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=(Math.acos((double)1/res));
 			l.setText(result+"");
 		}
 		else if(b_6.isSelected())
 		{
-			//double rad =Math.toRadians(res);
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=(Math.atan((double)1/res));
 			l.setText(result+"");
-		}
-//		
+		}	
 		else if(b7.isSelected())
 		{
-//			JTextField t=new JTextField();
-//			add (t);
-//			String root_str=t.getText();
-//			int root = Integer.parseInt(root_str);
-//			result=res^root;
-//			l.setText(result+"");
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=Math.sqrt(res);
 			l.setText(result+"");
 			
 		}
 		else if(b8.isSelected())
 		{
-//			JTextField t=new JTextField();
-//			add(t);
-//			String root_str=t.getText();
-//			int root = Integer.parseInt(root_str);
-//			result=res^(1/root);
-//			l.setText(result+"");
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=res*res;
 			l.setText(result+"");
 		}
 		else if(b9.isSelected())
 		{
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=Math.cbrt(res);
 			l.setText(result+"");
 		}
 		else if(b10.isSelected())
 		{
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=res*res*res;
 			l.setText(result+"");
 		}
 		else if(b11.isSelected())
 		{
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=Math.log10(res);
 			l.setText(result+"");			
 		}
 		else if(b12.isSelected())
 		{
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			result=Math.log(res);
 			l.setText(result+"");	
 		}
-		else
+		
+		else if(b13.isSelected())
 		{
+			l.setText(itp.Xevaluate(itp.inpo(itp.convert(str))));
+		}
+		
+		else if(b.isSelected())
+		{
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
 			l.setText(res+"");
-		}
-		
-		//l.setText(result+"");
-		
-	}
-	public static int evaluate(String expression)
-	{
-		char[] tokens = expression.toCharArray();
-
-		// Stack for numbers: 'values'
-		Stack<Integer> values = new
-							Stack<Integer>();
-
-		// Stack for Operators: 'ops'
-		Stack<Character> ops = new
-							Stack<Character>();
-
-		for (int i = 0; i < tokens.length; i++)
-		{
-			if (tokens[i] == ' ')
-				continue;
-
-			
-			if (tokens[i] >= '0' &&
-				tokens[i] <= '9')
-			{
-				StringBuffer sbuf = new
-							StringBuffer();
-				
-				
-				while (i < tokens.length &&
-						tokens[i] >= '0' &&
-						tokens[i] <= '9')
-					sbuf.append(tokens[i++]);
-				values.push(Integer.parseInt(sbuf.
-									toString()));
-				i--;
-			}
-
-			
-			else if (tokens[i] == '(')
-				ops.push(tokens[i]);
-
-			
-			else if (tokens[i] == ')')
-			{
-				while (ops.peek() != '(')
-				values.push(applyOp(ops.pop(),
-								values.pop(),
-								values.pop()));
-				ops.pop();
-			}
-
-			else if (tokens[i] == '+' ||
-					tokens[i] == '-' ||
-					tokens[i] == '*' ||
-						tokens[i] == '/')
-			{
-				while (!ops.empty() &&
-					hasPrecedence(tokens[i],
-									ops.peek()))
-				values.push(applyOp(ops.pop(),
-								values.pop(),
-								values.pop()));
-
-				ops.push(tokens[i]);
-			}
-		}
-
-		
-		while (!ops.empty())
-			values.push(applyOp(ops.pop(),
-							values.pop(),
-						values.pop()));
-
-		
-		return values.pop();
-	}
-
-	public static boolean hasPrecedence(
-						char op1, char op2)
-	{
-		if (op2 == '(' || op2 == ')')
-			return false;
-		if ((op1 == '*' || op1 == '/') &&
-			(op2 == '+' || op2 == '-'))
-			return false;
+		}		
 		else
-			return true;
-	}
-
-
-	public static int applyOp(char op,
-						int b, int a)
-	{
-		switch (op)
 		{
-		case '+':
-			return a + b;
-		case '-':
-			return a - b;
-		case '*':
-			return a * b;
-		case '/':
-			return a / b;
-		}
-		return 0;
+			double res = itp.evaluate(itp.inpo(itp.convert(str)));
+			l.setText(res+"");
+		}		
 	}
-
 }
